8a0cd7d73feb291fce8b2d90b561579719ecac09
estoledo <estoledo1@utpl.edu.ec>
2023-12-21 15:58:07 -0500

    problema 1

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -16,6 +16,31 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
+        String[] estudent = {"Jerry Ponce", "Gabriela Lewis",
+            "David Bell"};
+        double[] sumaNotas = new double[3];
+        double[] promedio = new double[3];
+
+        double suma = 0;
+
+        for (int f = 0; f < notas.length; f++) {
+            suma = 0;
+
+            for (int c = 0; c < notas[f].length; c++) {
+                suma = suma + notas[f][c];
+                sumaNotas[f] = suma;
+                 
+            }
+            promedio[f] = (sumaNotas[f]) / 4;
+        }
+       
+        for (int f = 0; f < notas.length; f++) {
+            System.out.printf("Estudiante: %s tiene un promedio de %.2f\n", estudent[f], promedio[f]);
+        }
+
     }
-    
+
 }
